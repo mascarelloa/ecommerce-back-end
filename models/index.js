a8c20dba2d8075938a08bff497293779a2c18e98
mascarelloa <amfm0793@gmail.com>
2021-04-29 18:30:27 -0400

    added files and structure, added models, began routing.

---
+++ models/index.js
@@ -0,0 +1,36 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+
+// Categories have many Products
+
+// Products belongToMany Tags (through ProductTag)
+
+// Tags belongToMany Products (through ProductTag)
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
+
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+})
+
+Product.belongsToMany(Tag, {
+  through: { model: ProductTag, unique: false }, as: "tagged_products"
+});
+
+Tag.belongsToMany(Product, {
+    through: { model: ProductTag, unique: false }, as: "product_tags"
+  });
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
